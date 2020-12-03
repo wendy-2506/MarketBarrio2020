588b4803f1b8285e47506104a87a44cd55d9177a
Jimmygs112 <19100935@ue.edu.pe>
2020-12-03 12:17:13 -0500

    Acabo de añadir unas clases al paquete Bean

---
+++ src/BEAN/Tienda.java
@@ -0,0 +1,41 @@
+package BEAN;
+public class Tienda {
+    private int idTienda;
+    private String descTienda;
+    private String direccion;
+
+    public Tienda() {
+    }
+
+    public Tienda(int idTienda, String descTienda, String direccion) {
+        this.idTienda = idTienda;
+        this.descTienda = descTienda;
+        this.direccion = direccion;
+    }
+
+    public int getIdTienda() {
+        return idTienda;
+    }
+
+    public void setIdTienda(int idTienda) {
+        this.idTienda = idTienda;
+    }
+
+    public String getDescTienda() {
+        return descTienda;
+    }
+
+    public void setDescTienda(String descTienda) {
+        this.descTienda = descTienda;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
