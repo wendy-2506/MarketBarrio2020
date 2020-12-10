d7077862968003d88a73553230674bcd5af4e108
jhemapca <75439401+jhemapca@users.noreply.github.com>
2020-12-10 11:32:40 -0500

    DATOOOOS

---
+++ DATOS.sql
@@ -0,0 +1,18 @@
+insert into Tipo_Empleado values (1,'Gerente de tienda')
+insert into Tipo_Empleado values (2,'Cajero')
+
+insert into Empleado values (1,'Mendoza', 'Wendy',1,'2020-12-10',1,'2020-12-10','wendym','ultrasecreto',1,1)
+insert into Empleado values (2,'Apaza', 'Jhoel',1,'2020-12-10',1,'2020-12-10','jhemapca','jhoelemir10',2,1)
+
+insert into Tipo_Cliente values (1,'Cliente natural')
+insert into Tipo_Cliente values (2,'Empresa')
+
+insert into Cliente values (1,1,2,'2020-12-10',2,'2020-12-10','ejemplo123@hotmail.com','967301439','San Miguel',1)
+insert into Cliente_Persona values (1,'Vega','Rodrigo',70812943)
+insert into Cliente values (2,2,2,'2020-12-10',2,'2020-12-10','empresa@gmail.com','962232158','La molina',1)
+insert into Cliente_Empresa values(2,'Empresa El Ejemplo','Mariano Ramirez','10164090588')
+
+insert into Categoria values(1,'Alimentos y Bebidas')
+insert into Categoria values(2,'Animales y Mascotas')
+insert into Categoria values(3,'Librería')
+insert into Categoria values(4,'Belleza y Cuidado Personal')
