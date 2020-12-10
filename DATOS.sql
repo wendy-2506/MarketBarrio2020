28374c596c7cea647c700b2d2eace74bf892ca2c
jhemapca <75439401+jhemapca@users.noreply.github.com>
2020-12-10 12:13:51 -0500

    Proveedores

--- DATOS.sql
+++ DATOS.sql
@@ -36,3 +36,10 @@ insert into Producto values(7,'Fanta 450ml',1,2,1.5,'Botella',1)
 insert into Producto values(8,'Sprite 500ml',1,2,1.5,'Botella',1)
 insert into Producto values(9,'Powerade 550ml',1,2,2.5,'Botella',1)
 insert into Producto values(10,'Aquarius 500ml',1,2,1,'Botella',1)
+
+insert into Proveedor values(1, 'Ucp Backus y Jonhston', '1798285937001', 'Alonso Fernandez', 'Alonso de Molina 543 - Santiago de Surco', '01-765678')
+insert into Proveedor values(2, 'Aje Group', '2698685737201', 'Maria Sanchez', 'Los Geranios 123 - San Miguel', '01-456783')
+insert into Proveedor values(3, 'Coca Cola Peru', '459828123001', 'John Smith', 'Av. Los Girasoles 6745 - Callao', '01-123456')
+insert into Proveedor values(4, 'Alicorp', '123145123544', 'Juan Perez', 'Micaela Bastidas 546 - San Isidro', '01-123451')
+insert into Proveedor values(5, 'Makro Supermayorista S.A.', '20492092313', 'Jose Passano', 'Av. Jorge Chavez Nro. 1218 - Santiago de Surco', '01-6348000')
+
