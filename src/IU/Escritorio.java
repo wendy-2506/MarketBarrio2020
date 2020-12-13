cc124d0a11b70b58010807805d08c80bfa985ddf
wendy-2506 <75333346+wendy-2506@users.noreply.github.com>
2020-12-12 20:19:35 -0500

    escritorio con login integrado xd

--- src/IU/Escritorio.java
+++ src/IU/Escritorio.java
@@ -1,9 +1,13 @@
 package IU;
 
+import DAO.EmpleadoDAO;
+import java.sql.Connection;
+import java.sql.Statement;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Escritorio extends javax.swing.JFrame {
-    
+    EmpleadoDAO e;
     public Escritorio() {
         initComponents();
         this.setLocationRelativeTo(null);
@@ -11,9 +15,10 @@ public class Escritorio extends javax.swing.JFrame {
         this.loginpanel.setAlignmentY(CENTER_ALIGNMENT);
         this.jLabel1.setAlignmentX(CENTER_ALIGNMENT);
         this.jLabel1.setAlignmentY(CENTER_ALIGNMENT);
-        this.jLabel1.setLocation(420, 50);
+        this.jLabel1.setLocation(450, 85);
         this.loginpanel.setLocation(580, 200);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        e=new EmpleadoDAO();
     }
     
     @SuppressWarnings("unchecked")
@@ -26,13 +31,11 @@ public class Escritorio extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        txtuser = new javax.swing.JTextField();
-        txtpasw = new javax.swing.JPasswordField();
+        txtusuario = new javax.swing.JTextField();
+        txtcontra = new javax.swing.JPasswordField();
+        btnIngLog = new javax.swing.JButton();
         menuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
-        openMenuItem = new javax.swing.JMenuItem();
-        editMenu = new javax.swing.JMenu();
-        subMenVenta = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -48,24 +51,36 @@ public class Escritorio extends javax.swing.JFrame {
 
         jLabel4.setText("Contraseña:");
 
+        btnIngLog.setText("Ingresar");
+        btnIngLog.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIngLogActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout loginpanelLayout = new javax.swing.GroupLayout(loginpanel);
         loginpanel.setLayout(loginpanelLayout);
         loginpanelLayout.setHorizontalGroup(
             loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(loginpanelLayout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addGroup(loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(txtpasw, javax.swing.GroupLayout.DEFAULT_SIZE, 104, Short.MAX_VALUE)
-                    .addComponent(txtuser))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, loginpanelLayout.createSequentialGroup()
                 .addContainerGap(82, Short.MAX_VALUE)
                 .addComponent(jLabel2)
                 .addGap(76, 76, 76))
+            .addGroup(loginpanelLayout.createSequentialGroup()
+                .addGroup(loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(loginpanelLayout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addGroup(loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtcontra, javax.swing.GroupLayout.DEFAULT_SIZE, 104, Short.MAX_VALUE)
+                            .addComponent(txtusuario)))
+                    .addGroup(loginpanelLayout.createSequentialGroup()
+                        .addGap(85, 85, 85)
+                        .addComponent(btnIngLog)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         loginpanelLayout.setVerticalGroup(
             loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -75,38 +90,27 @@ public class Escritorio extends javax.swing.JFrame {
                 .addGap(38, 38, 38)
                 .addGroup(loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(txtuser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtusuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(41, 41, 41)
                 .addGroup(loginpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel4)
-                    .addComponent(txtpasw, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(98, Short.MAX_VALUE))
+                    .addComponent(txtcontra, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(33, 33, 33)
+                .addComponent(btnIngLog)
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         Escritorio.add(loginpanel);
         loginpanel.setBounds(210, 90, 250, 260);
 
         fileMenu.setMnemonic('f');
-        fileMenu.setText("Mantenimiento");
-
-        openMenuItem.setMnemonic('o');
-        openMenuItem.setText("Producto");
-        fileMenu.add(openMenuItem);
-
-        menuBar.add(fileMenu);
-
-        editMenu.setMnemonic('e');
-        editMenu.setText("Transacción");
-
-        subMenVenta.setText("Venta");
-        subMenVenta.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenVentaActionPerformed(evt);
+        fileMenu.setText("Cerrar");
+        fileMenu.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                fileMenuMouseClicked(evt);
             }
         });
-        editMenu.add(subMenVenta);
-
-        menuBar.add(editMenu);
+        menuBar.add(fileMenu);
 
         setJMenuBar(menuBar);
 
@@ -124,13 +128,34 @@ public class Escritorio extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void subMenVentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenVentaActionPerformed
-        FrmVenta fventa = new FrmVenta();
-        this.Escritorio.add(fventa);
-        fventa.setVisible(true);
-        fventa.setSize(548,766);
-    }//GEN-LAST:event_subMenVentaActionPerformed
+    private void btnIngLogActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIngLogActionPerformed
+        String usr =this.txtusuario.getText();
+        String pw = this.txtcontra.getText();
 
+        if(valida(usr,pw) == true){
+            EscritorioPrincipal escP = new EscritorioPrincipal();
+            JOptionPane.showMessageDialog(this, "BIEN XD");
+            escP.setVisible(true);
+            escP.setSize(1366, 768);
+            this.dispose();
+        }else{
+            JOptionPane.showMessageDialog(this, "Error: Usted no se encuentra Registrado en el sistema");
+        }
+
+    }//GEN-LAST:event_btnIngLogActionPerformed
+
+    private void fileMenuMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_fileMenuMouseClicked
+        this.dispose();
+    }//GEN-LAST:event_fileMenuMouseClicked
+
+    public boolean valida(String User,String Password){
+        boolean dec=false;
+            if(e.Validame(User, Password)==true){
+                dec=true;
+            }
+        return dec;
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -169,7 +194,7 @@ public class Escritorio extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane Escritorio;
-    private javax.swing.JMenu editMenu;
+    private javax.swing.JButton btnIngLog;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -177,10 +202,8 @@ public class Escritorio extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel loginpanel;
     private javax.swing.JMenuBar menuBar;
-    private javax.swing.JMenuItem openMenuItem;
-    private javax.swing.JMenuItem subMenVenta;
-    private javax.swing.JPasswordField txtpasw;
-    private javax.swing.JTextField txtuser;
+    private javax.swing.JPasswordField txtcontra;
+    private javax.swing.JTextField txtusuario;
     // End of variables declaration//GEN-END:variables
 
 }
